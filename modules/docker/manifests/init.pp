69b9779dc027c6b39f29d35aadad6e6f7f7f7109
Bruno Pessanha <bruno.pessanha@imc.com>
2018-10-12 17:23:53 +0200

    adding docker

--- modules/docker/manifests/init.pp
+++ modules/docker/manifests/init.pp
@@ -1,8 +1,11 @@
 class docker {
-  yumrepo { "docker-ce":
+  yumrepo { 'docker-ce':
     baseurl  => "https://download.docker.com/linux/fedora/28/x86_64/stable",
     descr    => "Docker CE",
     enabled  => 1,
     gpgcheck => 0
+  }
+  package { 'docker-ce':
+    ensure => 'installed'
   }
 }
